ce8807c0ca34eaa2e1182b25bb88bc25c2ed9c90
gyoyeon Kim <gy2121@naver.com>
2024-10-17 16:06:17 +0900

    test1

--- frontend/src/Main/Main.js
+++ frontend/src/Main/Main.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Main.css';
 
+
 function Main() {
     const handleClick = () => {
         alert('로그인');
@@ -13,12 +14,26 @@ function Main() {
     return (
       <div className="Main">
         <header className="Main-header">
-          <button className="custom-button" onClick={handleClick}>
-            로그인
-          </button>
-          <button className="custom-button" onClick={handleClick2}>
-            회원가입
-          </button>
+          {/* 왼쪽 그리드 */}
+          <div className="left-grid">
+            <div className="Title" >
+              Trip Plan
+              <div className="Content" >
+                손쉬운 여행 계획 짜기
+              </div>
+            </div> 
+            <button className="custom-button" onClick={handleClick}>
+              로그인
+            </button>
+            <button className="custom-button" onClick={handleClick2}>
+              회원가입
+            </button>
+          </div>
+
+        {/* 오른쪽 그리드 */}
+          <div className="right-grid">
+            <div>서동재ㅡㅡ</div> {/* 여기 오른쪽 그리드에 "안녕" 추가 */}
+          </div>
         </header>
       </div>
     );
