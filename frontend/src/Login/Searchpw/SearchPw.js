8076f0cbbd395b86cbd57b3d7e9e9e5628a2fa0a
gyoyeon Kim <gy2121@naver.com>
2024-11-05 13:54:12 +0900

    Add Searchpw js

--- frontend/src/Login/Searchpw/SearchPw.js
+++ frontend/src/Login/Searchpw/SearchPw.js
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import axios from 'axios'
 import { TextField, Box } from '@mui/material';
-import './Search.css';
+import './SearchPw.css';
 
 function SearchPw() {
 
